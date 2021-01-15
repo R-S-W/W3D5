d5e0929a12370a9c9cf787dc9fcf6843d8fd146e
Christian <cwooddel@u.rochester.edu>
2021-01-15 17:03:57 -0500

    started knight travails

--- KnightsTravails/knights_travails.rb
+++ KnightsTravails/knights_travails.rb
@@ -1,5 +1,22 @@
-
-
+require_relative "../PolyTreeNodeProject/skeleton/lib/00_tree_node.rb"
 class KnightPathFinder
+
+    def self.valid_moves(pos)
+        move_arr = []
+        gallup_arr = [[2, 1], [2, -1], [-2, 1], [-2, -1], [1, 2], [-1, 2], [1, -2], [-1, -2]]
+    end 
+
+    def initialize(pos)
+        @position = pos 
+        @root_node = PolyTreeNode.new(pos)
+
+    end 
+
+    def find_path(pos)
+
+    end 
     
+    def new_move_positions
+
+    end 
 end
