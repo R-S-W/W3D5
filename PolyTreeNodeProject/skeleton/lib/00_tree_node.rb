2dfa8190ccff8a6dad89937694996233546dd94f
Ray <raysidarta@gmail.com>
2021-01-15 14:03:04 -0500

    Create beginning of PolyTreeNode Class

--- PolyTreeNodeProject/skeleton/lib/00_tree_node.rb
+++ PolyTreeNodeProject/skeleton/lib/00_tree_node.rb
@@ -1,3 +1,33 @@
 class PolyTreeNode
+    attr_reader  :parent, :children, :value
+
+    def initialize(value)
+        @value = value
+        @parent = nil
+        @children  = []
+
+    end
+
+    def parent=(parent_node)
+
+        self.parent.remove_child(self)
+
+        self.parent = parent_node
+        parent_node.children << self
+
+        
+        
+        
+    end
+
+    def add_child(child_node)
+        child_node.parent = self
+
+        
+    end
+
+    def remove_child
+    end
+
 
 end
